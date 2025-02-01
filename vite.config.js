f2f422e2a42e9a61aa8aa5bacdae819ea1ae563f
v4nilla1ce <v4nilla@tuta.io>
2025-02-01 22:08:46 +0100

    change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
-  plugins: [react()],
-  base: "/imageboard/", // Set the correct base path for GitHub Pages
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Example: Separate React into its own chunk
+        },
+      },
+    },
+  },
 });
