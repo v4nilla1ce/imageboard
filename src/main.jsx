0d7dba18635b355985694dc51a14dc02273b494a
v4nilla1ce <v4nilla@tuta.io>
2025-02-09 18:01:50 +0100

    change main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css'; // Globale Styles
+import App from './App.jsx'; // Hauptkomponente mit Routing
 
+// Render der React-App im Root-Div
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
